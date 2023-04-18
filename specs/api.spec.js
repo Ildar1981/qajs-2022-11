11a82298e1fbeb9eb829fe4d97724b875d7e2226
Ildar1981 <fahriyevi@yandex.ru>
2023-04-18 22:38:10 +0300

    added api

---
+++ specs/api.spec.js
@@ -0,0 +1,9 @@
+import {fetch} from "node-fetch"
+import {describe, expect, test} from '@jest/globals'
+
+test('GET request', async () => {
+    const response = fetch('https://bookstore.demoqa.com/swagger/');
+    const data = await response.json();
+    console.log(response.status)
+    console.log(data)
+});
