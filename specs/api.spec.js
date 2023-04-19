a6088ab53340100b74726b82702127178ea85195
Ildar1981 <fahriyevi@yandex.ru>
2023-04-19 08:48:18 +0300

    added dependencies

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch"
+import fetch from 'node-fetch'
 import {describe, expect, test} from '@jest/globals'
 
 test('GET request', async () => {
@@ -7,3 +7,83 @@ test('GET request', async () => {
     console.log(response.status)
     console.log(data)
 });
+
+test('create user pass login', async () => {
+    const response = await fetch( 'https://bookstore.demoqa.com/swagger/',
+    {method: 'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        login:'user',
+        password: 12345,
+    })
+})
+const data = await response.json();
+console.log(data)
+expect(data.brand).toEqual('macintosh')
+})
+
+test('create user pass login', async () => {
+    const response = await fetch( 'https://bookstore.demoqa.com/swagger/',
+    {method: 'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        login:'user',
+        password: 12345,
+    })
+})
+const data = await response.json();
+console.log(data)
+expect(data.brand).toEqual('macintosh')
+})
+
+test('create user pass login', async () => {
+    const response = await fetch( 'https://bookstore.demoqa.com/swagger/',
+    {method: 'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        login:'user',
+        password: 12345,
+    })
+})
+const data = await response.json();
+console.log(data)
+expect(data.brand).toEqual('macintosh')
+})
+
+test('create user pass login', async () => {
+    const response = await fetch( 'https://bookstore.demoqa.com/swagger/',
+    {method: 'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        login:'user',
+        password: 12345,
+    })
+})
+const data = await response.json();
+console.log(data)
+expect(data.brand).toEqual('macintosh')
+})
+
+test('create user pass login', async () => {
+    const response = await fetch( 'https://bookstore.demoqa.com/swagger/',
+    {method: 'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        login:'user',
+        password: 12345,
+    })
+})
+const data = await response.json();
+console.log(data)
+expect(data.brand).toEqual('macintosh')
+})
